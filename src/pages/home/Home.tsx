90bc9e95e6f420b933dc153089ca96738778e296
Renan Paulino <dev.renanpaulino@gmail.com>
2026-02-26 13:20:57 -0300

    refactor: ajuste de responsividade, alinhamento do header e otimização de SVGs

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Header } from '../../components/layout/Header'
+import React from "react";
+import { Header } from "../../components/layout/Header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <h1 className="font-fredoka font-bold text-darkgreen text-8xl">
+        Título em Fredoka e Verde Escuro
+      </h1>
+      <p className="font-sans text-orange">Parágrafo em Poppins e Laranja</p>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
