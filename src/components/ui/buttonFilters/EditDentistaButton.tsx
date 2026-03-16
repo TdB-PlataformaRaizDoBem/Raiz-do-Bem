33b34aa117a14cd708db8806252a1e9f3c4a8a5d
Renan Paulino <dev.renanpaulino@gmail.com>
2026-03-16 15:42:58 -0300

    feat(admin): gestão de dentistas e fluxo de validação profissional

---
+++ src/components/ui/buttonFilters/EditDentistaButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "../Button";
+import { Modal } from "../Modal";
+import { type Dentista } from "../../../data/dentistasData";
+import UpdateDentista from "../../forms/update/UpdateDentista";
+
+type EditDentistaButtonProps = {
+  user: Dentista;
+};
+
+const EditDentistaButton = ({ user }: EditDentistaButtonProps) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setOpen(true)} className="flex-1 md:flex-none">
+        Editar Dados
+      </Button>
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <UpdateDentista
+          initialData={user} 
+          onSuccess={() => setOpen(false)} 
+        />
+      </Modal>
+    </>
+  );
+};
+
+export default EditDentistaButton;
