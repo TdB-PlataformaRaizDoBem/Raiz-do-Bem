0f99be56d1cc627cec752e3e080ba796a747bcf2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-10 20:48:13 -0300

    refactor(): atualizando notificação para designações

--- src/hooks/useDesignacao.tsx
+++ src/hooks/useDesignacao.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { designacaoData } from "../data/designacaoData";
 import { type Beneficiario } from "../data/beneficiariosData";
 import { type Dentista, dentistasMock } from "../data/dentistasData"; // Importe o dentistasMock aqui
+import { useNotification } from "./useNotification";
 
 export const useDesignacao = () => {
   const [pendentes, setPendentes] = useState<Beneficiario[]>(designacaoData);
   const [beneficiarioEmFoco, setBeneficiarioEmFoco] = useState<Beneficiario | null>(null);
   const [dentistaSelecionado, setDentistaSelecionado] = useState<Dentista | null>(null);
   const [acaoPendente, setAcaoPendente] = useState<string | null>(null);
+
+  const { showNotification } = useNotification()
 
   const solicitarDesignacao = (b: Beneficiario, d: Dentista) => {
     setBeneficiarioEmFoco(b);
@@ -19,7 +22,7 @@ export const useDesignacao = () => {
     if (!beneficiarioEmFoco) return;
     setPendentes(prev => prev.filter(b => b.id !== beneficiarioEmFoco.id));
     setAcaoPendente(null);
-    alert("Designação realizada com sucesso!");
+    showNotification("Designação Concluida com Sucesso!")
   };
 
   return {
@@ -33,7 +36,6 @@ export const useDesignacao = () => {
   };
 };
 
-// --- ADICIONE ESTA FUNÇÃO AQUI EMBAIXO ---
 export const buscarDentistasProximos = (beneficiario: Beneficiario): Dentista[] => {
   if (!beneficiario) return [];
 
